39d348a2635ecda60152184b809f3838a63b5384
IhorBurylkin <burilkin90@gmail.com>
2025-02-20 20:46:55 +0100

    task 4.3

--- task4.3.py
+++ task4.3.py
@@ -3,7 +3,7 @@ import random
 list_1 = []
 
 for i in range(0, random.randint(3, 10)):
-    list_1.append(random.randint(0, 100))
+    list_1.append(random.randint(0, 10))
 
 result = [list_1[0], list_1[2], list_1[-2]]    
 print(result)
