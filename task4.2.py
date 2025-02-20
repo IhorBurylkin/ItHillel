4baa805b90bc05277f9cb1ee7cc4ed119cd9c2ed
IhorBurylkin <burilkin90@gmail.com>
2025-02-20 19:48:22 +0100

    task 4.2

--- task4.2.py
+++ task4.2.py
@@ -1,8 +1,12 @@
 list_1 = [0, 1, 7, 2, 4, 8]
 result = 0
 
-for i in range(0, len(list_1), 2):
-    result += list_1[i]
+if not list_1:
+    result = 0
+else:
+    for i in range(0, len(list_1), 2):
+        result += list_1[i]
 
-result *= list_1[len(list_1) - 1]
+    result *= list_1[len(list_1) - 1]
+
 print(result)
