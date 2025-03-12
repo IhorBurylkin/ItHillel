b5fcce762f1363c5cc1addb3dd11061ff89ca160
IhorBurylkin <burilkin90@gmail.com>
2025-03-12 07:35:53 +0100

    task9.1

--- task9.1.py
+++ task9.1.py
@@ -5,5 +5,5 @@ def popular_words (text, words):
 
     return result    
 
-assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near'])
+assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }
 print('OK')
