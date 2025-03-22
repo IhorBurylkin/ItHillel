14042471a04ce27304aa513f5a3faa110d091489
IhorBurylkin <burilkin90@gmail.com>
2025-03-22 21:44:38 +0100

    task12.1

--- task12.1.py
+++ task12.1.py
@@ -8,7 +8,7 @@ def delete_html_tags(html_file="draft.html", result_file='cleaned.txt'):
 
     without_tags = re.sub(r'<[^>]+>', '', text) 
     for line in without_tags.splitlines():
-        result.append((line + "\n")) if line.strip() else None
+        result.append(line + "\n") if line.strip() else None
 
     with codecs.open(result_file, 'w', 'utf-8') as outfile:
         outfile.write(''.join(result))
