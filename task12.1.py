fa1f4d0cc882e389c3ee73cf71990c8cc9088965
IhorBurylkin <burilkin90@gmail.com>
2025-03-22 21:42:46 +0100

    task12.1

--- task12.1.py
+++ task12.1.py
@@ -1,14 +1,16 @@
-import codecs
+import codecs, re
 
 def delete_html_tags(html_file="draft.html", result_file='cleaned.txt'):
     result = []
     
-    with codecs.open(html_file, 'r', 'utf-8') as infile: 
-        for line in infile.readlines():
-            tags = ''.join([char for char in line if char not in "<>"])
-            result.append(tags) if tags.strip() else None
+    with codecs.open(html_file, 'r', 'utf-8') as infile:
+        text = infile.read()
+
+    without_tags = re.sub(r'<[^>]+>', '', text) 
+    for line in without_tags.splitlines():
+        result.append((line + "\n")) if line.strip() else None
 
     with codecs.open(result_file, 'w', 'utf-8') as outfile:
-            outfile.write(''.join(result))
+        outfile.write(''.join(result))
 
 delete_html_tags()
